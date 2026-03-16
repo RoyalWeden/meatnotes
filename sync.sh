789cec2b868c8dfda0bba014b23fbe848526b931
Michael Kougang <roy.weden@yahoo.com>
2026-03-16 00:13:29 -0700

    Quartz sync: Mar 16, 2026, 12:13 AM

--- sync.sh
+++ sync.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-source ~/.zprofile
-source ~/.zshrc 2>/dev/null || true
-export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH
+eval "$(/opt/homebrew/bin/brew shellenv zsh)"
+export PATH=/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:$PATH
 cd "/Users/roywe/Library/Mobile Documents/com~apple~CloudDocs/Octarine/workspaces/bible"
 npx quartz sync

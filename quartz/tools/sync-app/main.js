94e4c4266664e2c37f9e1c41fe54a1708232e978
Michael Kougang <roy.weden@yahoo.com>
2026-04-08 16:21:59 -0700

    Quartz sync: Apr 8, 2026, 4:21 PM

--- quartz/tools/sync-app/main.js
+++ quartz/tools/sync-app/main.js
@@ -87,14 +87,6 @@ app.whenReady().then(() => {
     }
   }, 10_000);
 
-  // CLI flag: --bg-sync auto-triggers BibleGateway sync after startup
-  if (process.argv.includes('--bg-sync')) {
-    console.log('[Main] --bg-sync flag detected, auto-triggering BG sync in 3s...');
-    setTimeout(() => {
-      const { runBGSync } = require('./bg-controller');
-      runBGSync();
-    }, 3000);
-  }
 });
 
 app.on('window-all-closed', (e) => e.preventDefault());
